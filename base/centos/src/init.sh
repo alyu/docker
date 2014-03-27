78dab52a9bfcca72a2ecc6acd10b62f484240463
Alex Yu <alex@alexyu.se>
2014-03-27 22:19:49 +0900

    Fix sshd for centos base image

--- base/centos/src/init.sh
+++ base/centos/src/init.sh
@@ -3,4 +3,7 @@ set -x
 # mount fix
 grep -v rootfs /proc/mounts > /etc/mtab
 
+# generate ssh keys
+[[ ! -f /etc/ssh/ssh_host_rsa_key  ]] && service sshd start && service sshd stop
+
 /usr/bin/supervisord
